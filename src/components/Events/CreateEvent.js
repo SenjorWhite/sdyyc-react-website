79684eebaffaa1a7e9dc0d1c1c83afda6a63caf2
senjorwhite <senjorwhite@gmail.com>
2019-05-23 01:46:57 -0600

    Added: Build Basic view of create event form.

--- src/components/Events/CreateEvent.js
+++ src/components/Events/CreateEvent.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import '../../styles/CreateEvent.scss';
+import "react-datepicker/dist/react-datepicker.css";
 
-export default function CreateEvent() {
-    return (
-        <div>
-            This is Create Event Page
-        </div>
-    )
+export default class CreateEvent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            stateDate: new Date()
+        }
+    }
+
+    handleChange = (date) => {
+        this.setState({
+            startDate: date
+        });
+    }
+
+    render() {
+        return (
+            <div className="CreateEvent">
+                <form className="create-form">
+                    <label htmlFor="eventTitle">Title:</label>
+                    <input type="text" id="eventTitle" name="title" placeholder="The event title.." />
+                    <label htmlFor="datePicker">Date:</label>
+                    <DatePicker id="datePicker"
+                        selected={this.state.startDate}
+                        onChange={this.handleChange}
+                        showTimeSelect
+                        dateFormat="Pp"
+                        timeIntervals="10"
+                    />
+                    <label htmlFor="content">Content:</label>
+                    <textarea placeholder="Describe your event.." />
+                    <label htmlFor="invitee">Invitee:</label>
+                    <div className="button-container">
+                        <button>
+                            <i class="material-icons">
+                                undo
+                            </i>
+                            Back
+                        </button>
+                        <button>Next
+                        <i className="material-icons">
+                                send
+                        </i>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
 }
