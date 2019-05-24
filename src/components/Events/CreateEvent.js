1295ccc8104f9540fd0d2441777b2f8877ee819c
senjorwhite <senjorwhite@gmail.com>
2019-05-23 22:32:04 -0600

    Added: Built basic invitee in the event form.

--- src/components/Events/CreateEvent.js
+++ src/components/Events/CreateEvent.js
@@ -7,7 +7,19 @@ export default class CreateEvent extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            stateDate: new Date()
+            stateDate: new Date(),
+            invitees: [
+                {
+                    "_id": "5cd9c1ec779d877ae81b63a6",
+                    "displayName": "Senjor White",
+                    "picture": "https://lh5.googleusercontent.com/-Res8JrplBFg/AAAAAAAAAAI/AAAAAAAACFU/ATTokUN3Yck/photo.jpg"
+                },
+                {
+                    "_id": "5ce7652f3f2afd550ca65181",
+                    "displayName": "White Senjor",
+                    "picture": "https://lh6.googleusercontent.com/-Ig4gXW4-b1o/AAAAAAAAAAI/AAAAAAAAAAo/vgVKidemxU0/photo.jpg"
+                }
+            ]
         }
     }
 
@@ -17,10 +29,30 @@ export default class CreateEvent extends Component {
         });
     }
 
+    handleSubmit = () => {
+        console.log("Submit");
+    }
+
+    renderInvitees = () => {
+        let invitees = this.state.invitees.map((invitee, index) => {
+            return (
+                <div className="invitee" key={index}>
+                    <button>
+                        <img src={invitee.picture} alt={invitee.displayName}></img>
+                    </button>
+                    <label>
+                        {invitee.displayName}
+                    </label>
+                </div>
+            )
+        });
+        return invitees;
+    }
+
     render() {
         return (
             <div className="CreateEvent">
-                <form className="create-form">
+                <div className="create-form">
                     <label htmlFor="eventTitle">Title:</label>
                     <input type="text" id="eventTitle" name="title" placeholder="The event title.." />
                     <label htmlFor="datePicker">Date:</label>
@@ -34,20 +66,24 @@ export default class CreateEvent extends Component {
                     <label htmlFor="content">Content:</label>
                     <textarea placeholder="Describe your event.." />
                     <label htmlFor="invitee">Invitee:</label>
+                    <div className="invitees">
+                        {this.renderInvitees()}
+                    </div>
                     <div className="button-container">
                         <button>
-                            <i class="material-icons">
+                            <i className="material-icons">
                                 undo
                             </i>
                             Back
                         </button>
-                        <button>Next
+                        <button type="submit">
+                            Next
                         <i className="material-icons">
                                 send
                         </i>
                         </button>
                     </div>
-                </form>
+                </div>
             </div>
         )
     }
